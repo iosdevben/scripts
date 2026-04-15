90d64782ef8ddac062a0829ac3e025abe0c97b66
Ben Thomas <ben.thomas@carsales.com.au>
2026-04-16 08:27:34 +1000

    Clean up argument that was removed earlier

--- sims.sh
+++ sims.sh
@@ -175,7 +175,7 @@ print_help() {
 
     local allSupportedOSVersions=($oldestOS $previousOS $currentOS $nextOS)
 
-    echo "\nUsage: recreate_simulator_devices [[-o] [-2] [-1] [-0 min|all] [-n min|all] [-w min|all]] | [-a min|all]"
+    echo "\nUsage: recreate_simulator_devices [[-o] [-2] [-1] [-0 min|all] [-n min|all] | [-a min|all]"
     echo "\nSet \$PROXYING_CERTIFICATE to the location of the proxy root certificate if you want to automatically install one to each simulator"
     echo
     echo "Options:"
